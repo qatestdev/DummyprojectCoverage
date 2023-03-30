654f282f27eb4328c4ebb4d28c8c43c8874352a7
olgamaluhacodacy <olga.maluha@codacy.com>
2023-03-30 10:58:35 +0200

    Added happy method

--- src/main/java/utils/Monkey.java
+++ src/main/java/utils/Monkey.java
@@ -5,4 +5,8 @@ public class Monkey {
     String howManyMonkeysICanSee(Integer number) {
         return number + " monkeys I can see";
     }
+
+    Boolean happyMonkey(){
+        return true;
+    }
 }
