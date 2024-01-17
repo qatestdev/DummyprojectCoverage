ded34efba492d6d453e675edc2e929cf1a4bd210
devcodacytestuser <devcodacytestuser@codacy.com>
2024-01-17 16:05:27 +0000

    Update Cat.java

--- src/main/java/utils/Cat.java
+++ src/main/java/utils/Cat.java
@@ -4,6 +4,8 @@ public class Cat {
 
     Integer sayName(String name) {
         System.out.println("Hi, my name is" + name);
+        System.out.println("Hi, my name is" + name);
+
         return 11;
     }
 }
